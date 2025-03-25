d26f375aa49daa649f3867408c64f1b5dd615ea2
Lbnkz <lucasbaixonunes@gmail.com>
2025-03-25 12:17:59 -0300

    config for vercel

--- api/index.js
+++ api/index.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const routes = require("../src/routes/routes");
+const routes = require("../src/routes/routes"); // Ajuste conforme necessário
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
-
-// Rotas
 app.use(routes);
 
-// Inicia o servidor
 app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+
+module.exports = app; // Importante para a Vercel
